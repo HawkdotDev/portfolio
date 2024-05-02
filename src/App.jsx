eb75688aa861a31872ba92ee55d8d93c611cc351
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2024-05-03 00:43:15 +0530

    structured the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,25 @@ function App() {
 
   return (
     <>
-      <main className='w-screen flex'>
-        <div className="w-[20%] bg-slate-400 relative">
-          <h1 className="fixed h-screen p-2">
-            lol
-          </h1>
-
-        </div>
-        <div className="w-[80%] bg-slate-600">
-          {/* <h1>
-            gotcha
-          </h1> */}
-          <div className="w-full h-screen bg-yellow-500">
-            lendi
+      <main className="w-screen bg-[#c4bcb2] text-slate-700">
+        <nav className="w-screen relative">
+          <div className="w-full bg-[#c4bcb2] p-3 flex justify-center">The Daily Prophet</div>
+        </nav>
+        <div className="flex">
+          <div className="w-[20%] bg-[#c4bcb2] relative border-slate-700 border-[1px]">
+            <h1 className="sticky top-0 h-screen p-2">lol</h1>
           </div>
-          <div className="w-full h-screen bg-yellow-500">
-            lendi2
+          <div className="w-[80%] bg-[#c4bcb2] border-slate-700 border-t">
+            <div className="w-full h-screen bg-[#c4bcb2] p-3">lendi</div>
+            <div className="w-full h-screen bg-[#c4bcb2] p-3">lendi2</div>
           </div>
-          
         </div>
-        {/* <h1 className='m-2 text-lg'>Portfolio incoming</h1>
-        <div className="card">
-          <button className='bg-slate-700 p-2 mx-4 rounded-md border-white hover:border-pink-500 border-[2px]' onClick={() => setCount((count) => count + 1)}>
-            click to count
-          </button>
-            {count}
-        </div> */}
+        <div className="w-screen flex justify-center p-2">
+          leona
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
