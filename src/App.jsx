206e6a98cf60447aeba033c6bd9d681857b5584b
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2024-05-03 15:32:04 +0530

    implemented theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import owl from "./assets/icons/owl-b.svg";
+import owlB from "./assets/icons/owl-b.svg";
+import owlW from "./assets/icons/owl-w.svg";
 import moon from "./assets/icons/moon.svg";
 import sun from "./assets/icons/sun.svg";
 import { useState } from "react";
@@ -7,19 +8,36 @@ import { useState } from "react";
 
 function App() {
   const [IsLight, setIsLight] = useState(true);
+
+  const lightBG = "#c4bcb2";
+  const darkBG = "#4e4e4e";
+  const lightText = "#4e4e4e";
+  const darkText = "#c4bcb2";
+
+  const bgColor = IsLight ? lightBG : darkBG;
+  const textColor = IsLight ? lightText : darkText;
+
+  const owl = IsLight ? owlB : owlW;
+  // bg-[#c4bcb2] text-slate-700
   return (
     <>
-      <main className="w-screen bg-[#c4bcb2] text-slate-700">
+      <main
+        style={{
+          backgroundColor: bgColor,
+          color: textColor,
+        }}
+        className="w-screen text-lg"
+      >
         <nav className="w-screen relative flex px-3">
           <img src={owl} alt="logo" className="w-[30px] mx-2" />
-          <div className="w-full bg-[#c4bcb2] p-3 flex justify-center text-lg">
+          <div className="w-full bg-transparent p-2 flex justify-center text-3xl">
             The Daily Prophet
           </div>
           <button
             className="w-[35px] h-[35px] m-2 px-[1px] rounded-lg"
             onClick={() => {
               setIsLight(!IsLight);
-              console.log("theme changed to " + IsLight ? "moon" : "sun");
+              IsLight ? console.log("moon") : console.log("sun");
             }}
           >
             <img
@@ -30,15 +48,32 @@ function App() {
           </button>
         </nav>
         <div className="flex">
-          <div className="w-[20%] bg-[#c4bcb2] relative border-slate-700 border-r border-y">
-            <h1 className="sticky top-0 h-screen p-2">lol</h1>
+          <div
+            style={{
+              borderColor: textColor ? textColor : "#0000FF",
+            }}
+            className="w-[20%] bg-transparent relative border-r border-y"
+          >
+            <h1 className="sticky top-0 h-screen p-2">laura</h1>
           </div>
-          <div className="w-[80%] bg-[#c4bcb2] border-slate-700 border-t">
-            <div className="w-full h-screen bg-[#c4bcb2] p-3">lendi</div>
-            <div className="w-full h-screen bg-[#c4bcb2] p-3">lendi</div>
-            <div className="w-full h-screen bg-[#c4bcb2] p-3">lendi</div>
-            <div className="w-full h-screen bg-[#c4bcb2] p-3 border-slate-700 border-b">
-              lendi last
+          <div
+            style={{
+              borderColor: textColor ? textColor : "#0000FF",
+            }}
+            className="w-[80%] bg-transparent border-t"
+          >
+            <div className="w-full h-screen bg-transparent p-3">lendi</div>
+            <div className="w-full h-screen bg-transparent p-3">lendi 2</div>
+            <div className="w-full h-screen bg-transparent p-3">
+              return of lendi
+            </div>
+            <div
+              style={{
+                borderColor: textColor ? textColor : "#0000FF",
+              }}
+              className="w-full h-screen bg-transparent p-3 border-b"
+            >
+              the last lendi
             </div>
           </div>
         </div>
@@ -47,7 +82,8 @@ function App() {
           <a
             href="https://github.com/HawkdotDev"
             target="_blank"
-            className="ml-1 px-[6px] text-[#c4bcb2] bg-slate-700"
+            style={{ backgroundColor: textColor, color: bgColor }}
+            className="ml-[6px] px-[5px] text-[#c4bcb2] bg-slate-700"
           >
             HawkdotDev
           </a>
