81aae3a4d53f41994af99b764c79366cfbd476cf
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2026-07-16 13:45:31 +0530

    style: change ScrollMarquee font family to Space Grotesk

--- src/components/Navbar/NavbarHamburger.jsx
+++ src/components/Navbar/NavbarHamburger.jsx
@@ -9,7 +9,7 @@ const NavbarHamburger = React.forwardRef(({ onClick, onMouseEnter, onMouseLeave,
     >
       <button
         ref={buttonRef}
-        className="navbar-blend-element z-100 text-neutral-800 pl-1 pr-0 py-1 rounded-full transition-colors relative cursor-pointer"
+        className="z-100 text-neutral-800 pl-1 pr-0 py-1 rounded-full transition-colors relative cursor-pointer"
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
--- src/components/ScrollMarquee.jsx
+++ src/components/ScrollMarquee.jsx
@@ -115,7 +115,7 @@ export const ScrollMarquee = ({
         style={parallaxStyle}
       >
         <motion.div
-          className={`${scrollerClassName} flex whitespace-nowrap py-2 text-center font-sans text-4xl font-bold tracking-[-0.02em] drop-shadow md:text-[5rem] md:leading-20`}
+          className={`${scrollerClassName} flex whitespace-nowrap py-2 text-center font-grotesk text-4xl font-bold tracking-[-0.02em] drop-shadow md:text-[5rem] md:leading-20`}
           style={{ x, ...scrollerStyle }}
         >
           {spans}
